bad07db6ce38f981a0449cf7468d58f822cc964f
Abhishek <abhigupta0877@gmail.com>
2022-04-12 00:02:40 +0530

    mongo first program

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,31 +1,25 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
+const bookSchema  = new mongoose.Schema( {
+    bookName: {
         type: String,
-        unique: true,
         required: true
     },
-    emailId: String,
-    gender: {
+    authorName: {
         type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+        required: true
     },
-    age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
+    category: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
+module.exports = mongoose.model('Books', bookSchema ) 
 
 
-
-// String, Number
-// Boolean, Object/json, array
